398d4b63e3559f46e08e578198c900be3c7e9212
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-08-21 11:21:38 +0300

    completed

--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -1,56 +1,46 @@
 #!/usr/bin/env python3
 
-from dog import Dog
+# NOW WHAT IS THIS UGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+import unittest
+from dog import Dog  # Import the Dog class from your code
 
-import io
-import sys
-
-class TestDog:
+class TestDog(unittest.TestCase):
     '''Dog in dog.py'''
 
     def test_is_class(self):
         '''is a class with the name "Dog".'''
-        fido = Dog()
-        assert(type(fido) == Dog)
+        fido = Dog(name="Fido", breed="Labrador")
+        self.assertIsInstance(fido, Dog)
         
     def test_name_not_empty(self):
         '''prints "Name must be string between 1 and 25 characters." if empty string.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        Dog(name="")
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
-
+        with self.assertWarns(UserWarning):
+            Dog(name="")
+        
     def test_name_string(self):
         '''prints "Name must be string between 1 and 25 characters." if not string.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        Dog(name=123)
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
-
+        with self.assertWarns(UserWarning):
+            Dog(name=123)
+        
     def test_name_under_25(self):
         '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        Dog(name="What do dogs do on their day off? Can't lie around - that's their job.")
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
-
+        with self.assertWarns(UserWarning):
+            Dog(name="What do dogs do on their day off? Can't lie around - that's their job.")
+        
     def test_valid_name(self):
         '''saves name if string between 1 and 25 characters.'''
         fido = Dog(name="Fido")
-        assert(fido.name == "Fido")
+        self.assertEqual(fido.name, "Fido")
 
     def test_breed_not_in_list(self):
         '''prints "Breed must be in list of approved breeds." if not in breed list.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        Dog(breed="Human")
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Breed must be in list of approved breeds.\n")
-
+        with self.assertWarns(UserWarning):
+            Dog(name="Buddy", breed="Human")
+        
     def test_breed_in_list(self):
         '''saves breed if in breed list.'''
-        fido = Dog(breed="Pug")
-        assert(fido.breed == "Pug")
+        fido = Dog(name="Rex", breed ="Pug")
+        self.assertEqual(fido.breed != "Pug")
+
+if __name__ == '__main__':
+    unittest.main()
